dbe1bd2f3d87af491d372525fb5655c5e9d10bb8
Elena Ignatenko <Walden-ea@yandex.ru>
2022-11-18 13:28:49 +0300

    fix analyze

---
+++ lab2_1/src/main.rs
@@ -0,0 +1,5 @@
+mod solvers;
+
+fn main() {
+    solvers::unwrap1(solvers::_uber_solve(2,10000001));
+}
---
+++ lab2_1/target/debug/deps/lab1.d
@@ -0,0 +1,6 @@
+C:\Users\Elena\Documents\вуз\4 курс\интеллектуальные системы\IS_assignments\lab1\target\debug\deps\lab1.exe: src\main.rs src\solvers.rs
+
+C:\Users\Elena\Documents\вуз\4 курс\интеллектуальные системы\IS_assignments\lab1\target\debug\deps\lab1.d: src\main.rs src\solvers.rs
+
+src\main.rs:
+src\solvers.rs:
